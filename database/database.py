dcf93ad0312e361e5db0d548261fa6cb60af6d4e
Amrit Utsav <lbrock513@gmail.com>
2023-09-13 23:29:07 +0530

    database change to postgres

--- database/database.py
+++ database/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,7 +11,8 @@ load_dotenv()
 
 # mysql
 # FOR HOSTING SERVER
-SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{os.getenv("MYSQLUSER")}:{os.getenv("MYSQLPASSWORD")}@{os.getenv("MYSQLHOST")}:{os.getenv("MYSQLPORT")}/{os.getenv("MYSQLDATABASE")}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{os.getenv("USERNAME")}:{os.getenv("PASSWORD")}@{os.getenv("HOSTNAME")}:{os.getenv("PORT")}/{os.getenv("DATABASE")}'
+# SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{os.getenv("MYSQLUSER")}:{os.getenv("MYSQLPASSWORD")}@{os.getenv("MYSQLHOST")}:{os.getenv("MYSQLPORT")}/{os.getenv("MYSQLDATABASE")}'
 # SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{os.getenv("USERNAMEE")}:{os.getenv("PASSWORD")}@{os.getenv("HOST")}:{os.getenv("PORT")}/{os.getenv("DATABASE")}'
 # FOR LOCALHOST
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root@db/test'
